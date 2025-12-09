ab560c0ebc009660a6c72a1a8847096cca72ae38
SANJEEV KUMAR <sanju20july2002@gmail.com>
2025-12-09 23:27:56 +0530

    changed

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
-let IS_PROD = false;
+let IS_PROD = true;
 const server = IS_PROD ?
-    "https://pixel-call-backend.vercel.app/" :
+    "https://pixel-call-backend.onrender.com" :
 
     "http://localhost:8000"
 
